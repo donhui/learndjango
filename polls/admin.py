213a2d1a78ff1b88e9200dec2023d7a2e3ac0cd4
donhui <977675308@qq.com>
2015-11-20 09:34:02 +0800

    使用注册装饰器注册ModelAdmin类

--- polls/admin.py
+++ polls/admin.py
@@ -9,6 +9,7 @@ class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,              {'fields': ['question_text']}),
@@ -18,5 +19,3 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
-
-admin.site.register(Question, QuestionAdmin)
