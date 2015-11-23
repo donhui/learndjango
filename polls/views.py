0036f52723734ed225dff911bc12ae4acd08183c
donhui <977675308@qq.com>
2015-11-23 09:35:16 +0800

    修改View，不显示未来时间待发布的Question

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext, loader
 
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -14,11 +15,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the latest five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
